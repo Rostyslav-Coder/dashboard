5b0d76e5c7a90e3c423880c2c96a6dbd5ad1894c
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-27 00:31:04 +0200

    script.js updated

--- script.js
+++ script.js
@@ -1,126 +1,112 @@
 // Dashboard
 
+// Declare and initialize color theme
+const colorTheme = document.querySelector("#color-theme");
+
+// Declare and initialize main container
+const mainGridContainer = document.querySelector("#main-grid-container");
+
 // Declare and initialize sliders variables
-const mainGridContainer = document.querySelector('#main-grid-container');
-const dashboardLogo = document.querySelector('#dashboard');
-const home = document.querySelector('#home');
-const projects = document.querySelector('#projects');
-const cv = document.querySelector('#cv');
-const certificates = document.querySelector('#certificates');
-const contacts = document.querySelector('#contacts');
+const sliderButtons = {
+  dashboardLogo: document.querySelector("#dashboard"),
+  home: document.querySelector("#home"),
+  projects: document.querySelector("#projects"),
+  cv: document.querySelector("#cv"),
+  certificates: document.querySelector("#certificates"),
+  contacts: document.querySelector("#contacts"),
+};
 
+// Declare and initialize twisted slider content
 const twistedSlider = {
-  dashboard: (
-    '<span class="material-symbols-outlined">dashboard</span>'
-  ),
-  home: (
-    `<a href="#top-link">
+  dashboard: `<span class="material-symbols-outlined">
+      dashboard
+    </span>`,
+  home: `<a href="#top-link">
       <span class="material-symbols-outlined">home</span>
-    </a>`
-  ),
-  projects: (
-    `<a href="#project-link">
+    </a>`,
+  projects: `<a href="#project-link">
       <span class="material-symbols-outlined">terminal</span>
-    </a>`
-  ),
-  cv: (
-    `<a href="#curriculum-vitae-link">
+    </a>`,
+  cv: `<a href="#curriculum-vitae-link">
       <span class="material-symbols-outlined">developer_guide</span>
-    </a>`
-  ),
-  certificates: (
-    `<a href="#certificates-link">
+    </a>`,
+  certificates: `<a href="#certificates-link">
       <span class="material-symbols-outlined">license</span>
-    </a>`
-  ),
-  contacts: (
-    `<a href="#contact-link">
+    </a>`,
+  contacts: `<a href="#contact-link">
       <span class="material-symbols-outlined">contact_mail</span>
-    </a>`
-  ),
+    </a>`,
 };
 
+// Declare and initialize expanded slider content
 const expandedSlider = {
-  dashboard: (
-    '<span class="material-symbols-outlined">dashboard</span> Dashboard'
-  ),
-  home: (
-    `<a href="#top-link">
+  dashboard: `<span class="material-symbols-outlined">
+      dashboard
+    </span> Dashboard`,
+  home: `<a href="#top-link">
       <span class="material-symbols-outlined">home</span> Home
-    </a>`
-  ),
-  projects: (
-    `<a href="#project-link">
+    </a>`,
+  projects: `<a href="#project-link">
       <span class="material-symbols-outlined">terminal</span> Projects
-    </a>`
-  ),
-  cv: (
-    `<a href="#curriculum-vitae-link">
+    </a>`,
+  cv: `<a href="#curriculum-vitae-link">
       <span class="material-symbols-outlined">developer_guide</span> My CV
-    </a>`
-  ),
-  certificates: (
-    `<a href="#certificates-link">
+    </a>`,
+  certificates: `<a href="#certificates-link">
       <span class="material-symbols-outlined">license</span> Certificates
-    </a>`
-  ),
-  contacts: (
-    `<a href="#contact-link">
+    </a>`,
+  contacts: `<a href="#contact-link">
       <span class="material-symbols-outlined">contact_mail</span> Contacts
-    </a>`
-  ),
+    </a>`,
 };
 
-mainGridContainer.style.gridTemplateColumns = 'repeat(5, 1fr)';
+mainGridContainer.style.gridTemplateColumns = "repeat(5, 1fr)";
 
 // Sliders folding
-dashboardLogo.addEventListener('click', () => {
-  if (mainGridContainer.style.gridTemplateColumns === 'repeat(5, 1fr)') {
-    mainGridContainer.style.gridTemplateColumns = '58px repeat(4, 1fr)';
-    dashboardLogo.style.marginLeft = '-7px';
-    dashboardLogo.innerHTML = twistedSlider.dashboard;
-    home.innerHTML = twistedSlider.home;
-    projects.innerHTML = twistedSlider.projects;
-    cv.innerHTML = twistedSlider.cv;
-    certificates.innerHTML = twistedSlider.certificates;
-    contacts.innerHTML = twistedSlider.contacts;
+sliderButtons.dashboardLogo.addEventListener("click", () => {
+  if (mainGridContainer.style.gridTemplateColumns === "repeat(5, 1fr)") {
+    mainGridContainer.style.gridTemplateColumns = "58px repeat(4, 1fr)";
+    sliderButtons.dashboardLogo.style.marginLeft = "-7px";
+    sliderButtons.dashboardLogo.innerHTML = twistedSlider.dashboard;
+    sliderButtons.home.innerHTML = twistedSlider.home;
+    sliderButtons.projects.innerHTML = twistedSlider.projects;
+    sliderButtons.cv.innerHTML = twistedSlider.cv;
+    sliderButtons.certificates.innerHTML = twistedSlider.certificates;
+    sliderButtons.contacts.innerHTML = twistedSlider.contacts;
   } else {
-    mainGridContainer.style.gridTemplateColumns = 'repeat(5, 1fr)';
-    dashboardLogo.style.marginLeft = '';
-    dashboardLogo.innerHTML = expandedSlider.dashboard;
-    home.innerHTML = expandedSlider.home;
-    projects.innerHTML = expandedSlider.projects;
-    cv.innerHTML = expandedSlider.cv;
-    certificates.innerHTML = expandedSlider.certificates;
-    contacts.innerHTML = expandedSlider.contacts;
+    mainGridContainer.style.gridTemplateColumns = "repeat(5, 1fr)";
+    sliderButtons.dashboardLogo.style.marginLeft = "";
+    sliderButtons.dashboardLogo.innerHTML = expandedSlider.dashboard;
+    sliderButtons.home.innerHTML = expandedSlider.home;
+    sliderButtons.projects.innerHTML = expandedSlider.projects;
+    sliderButtons.cv.innerHTML = expandedSlider.cv;
+    sliderButtons.certificates.innerHTML = expandedSlider.certificates;
+    sliderButtons.contacts.innerHTML = expandedSlider.contacts;
   }
 });
 
 // Set The Thema When Loading The Page
-document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('theme') === 'light') {
-    document.documentElement.setAttribute('class', 'light');
-    document.getElementById('color-theme').innerHTML = (
-      '<img src="svg/moon.svg" alt="light mode logo">'
-    );
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") === "light") {
+    document.documentElement.setAttribute("class", "light");
+    document.getElementById("color-theme").innerHTML =
+      "<img src='svg/moon.svg' alt='light mode logo' />";
   } else {
-    document.documentElement.setAttribute('class', 'dark');
-    document.getElementById('color-theme').innerHTML = (
-      '<img src="svg/sun.svg" alt="dark mode logo">'
-    );
+    document.documentElement.setAttribute("class", "dark");
+    document.getElementById("color-theme").innerHTML =
+      "<img src='svg/sun.svg' alt='dark mode logo' />";
   }
 });
 
 // Switch the Themas on click
-// eslint-disable-next-line func-names
-document.getElementById('color-theme').addEventListener('click', function () {
-  if (document.documentElement.getAttribute('class') === 'light') {
-    document.documentElement.setAttribute('class', 'dark');
-    this.innerHTML = '<img src="svg/sun.svg" alt="dark mode logo">';
-    localStorage.setItem('theme', 'dark');
+colorTheme.addEventListener("click", () => {
+  if (document.documentElement.getAttribute("class") === "light") {
+    document.documentElement.setAttribute("class", "dark");
+    colorTheme.innerHTML = "<img src='svg/sun.svg' alt='dark mode logo' />";
+    localStorage.setItem("theme", "dark");
   } else {
-    document.documentElement.setAttribute('class', 'light');
-    this.innerHTML = '<img src="svg/moon.svg" alt="light mode logo">';
-    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute("class", "light");
+    colorTheme.innerHTML = "<img src='svg/moon.svg' alt='light mode logo' />";
+    localStorage.setItem("theme", "light");
   }
 });
